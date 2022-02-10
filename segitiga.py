9abdfc47f9f83abfa1517695b9c9367e3a975cb6
bintangkastella <99344460+bintangkastella@users.noreply.github.com>
2022-02-10 10:45:37 +0900

    tugas pbo
    5210411185/THUFAIL BINTANG KASTELLA

---
+++ segitiga.py
@@ -0,0 +1,12 @@
+#NAMA : THUFAIL BINTANG KASTELLA 
+#NPM  : 5210411185
+
+
+print("Program Menghitung luas segitiga")
+
+a=float(input("\nMasukan Nilai Alas :"))
+t=float(input("Masukan Nilai Tinggi :"))
+
+luas=0.5*a*t
+
+print("Tampilkan Luas Segitiga ="+str(luas))
